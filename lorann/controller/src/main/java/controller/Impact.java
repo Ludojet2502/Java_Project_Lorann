75b8e937c05274f6d0db7c25df0e838d3d3a7eed
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-06-02 10:52:34 +0200

    Structure impact OK

--- lorann/controller/src/main/java/controller/Impact.java
+++ lorann/controller/src/main/java/controller/Impact.java
@@ -10,7 +10,10 @@ import model.IEnergyBall;
 import model.IHBone;
 import model.ILorann;
 import model.IModel;
-import model.IMonster;
+import model.IMonster1;
+import model.IMonster2;
+import model.IMonster3;
+import model.IMonster4;
 import model.IOBone;
 import model.IPurse;
 import model.ISpell;
@@ -18,11 +21,23 @@ import model.IVBone;
 
 
 
-public class Impact implements IModel, ILorann, IMonster, ISpell, IHBone, IVBone, IOBone, IEnergyBall, IPurse, IDoorClosed, IDoorOpened{
-	 public void checkImpact( ILorann lorann, IMonster monster) {
+public class Impact implements IModel, ILorann, IMonster1, IMonster2, IMonster3, IMonster4, ISpell, IHBone, IVBone, IOBone, IEnergyBall, IPurse, IDoorClosed, IDoorOpened{
+	 	public void checkImpact( ILorann lorann, IMonster1 monster) {
 
         }
+	 	
+	 	public void checkImpact( ILorann lorann, IMonster2 monster) {
 
+        }
+	 	
+	 	public void checkImpact( ILorann lorann, IMonster3 monster) {
+
+        }
+	 	
+	 	public void checkImpact( ILorann lorann, IMonster4 monster) {
+
+        }
+	 	
 	    public void checkImpact( ILorann lorann, ISpell spell) {
 
 	    }
@@ -55,7 +70,19 @@ public class Impact implements IModel, ILorann, IMonster, ISpell, IHBone, IVBone
 
 	    }
 
-	    public void checkImpact( ISpell spell, IMonster monster) {
+	    public void checkImpact( ISpell spell, IMonster1 monster) {
+
+	    }
+	    
+	    public void checkImpact( ISpell spell, IMonster2 monster) {
+
+	    }
+	    
+	    public void checkImpact( ISpell spell, IMonster3 monster) {
+
+	    }
+	    
+	    public void checkImpact( ISpell spell, IMonster4 monster) {
 
 	    }
 
@@ -86,31 +113,115 @@ public class Impact implements IModel, ILorann, IMonster, ISpell, IHBone, IVBone
 
 	    }
 
-	    public void checkImpact( IMonster monster, IHBone hBone) {
+	    public void checkImpact( IMonster1 monster, IHBone hBone) {
+
+	    }
+	    
+	    public void checkImpact( IMonster2 monster, IHBone hBone) {
+
+	    }
+	    
+	    public void checkImpact( IMonster3 monster, IHBone hBone) {
+
+	    }
+	    
+	    public void checkImpact( IMonster4 monster, IHBone hBone) {
 
 	    }
 
-	    public void checkImpact( IMonster monster, IVBone vBone) {
+	    public void checkImpact( IMonster1 monster, IVBone vBone) {
+
+	    }
+	    
+	    public void checkImpact( IMonster2 monster, IVBone vBone) {
+
+	    }
+	    
+	    public void checkImpact( IMonster3 monster, IVBone vBone) {
+
+	    }
+	    
+	    public void checkImpact( IMonster4 monster, IVBone vBone) {
 
 	    }
 
-	    public void checkImpact( IMonster monster, IOBone oBone) {
+	    public void checkImpact( IMonster1 monster, IOBone oBone) {
+
+	    }
+	    
+	    public void checkImpact( IMonster2 monster, IOBone oBone) {
+
+	    }
+	    
+	    public void checkImpact( IMonster3 monster, IOBone oBone) {
+
+	    }
+	    
+	    public void checkImpact( IMonster4 monster, IOBone oBone) {
 
 	    }
 
-	    public void checkImpact( IMonster monster, IEnergyBall energyBall) {
+	    public void checkImpact( IMonster1 monster, IEnergyBall energyBall) {
+
+	    }
+	    
+	    public void checkImpact( IMonster2 monster, IEnergyBall energyBall) {
+
+	    }
+	    
+	    public void checkImpact( IMonster3 monster, IEnergyBall energyBall) {
+
+	    }
+	    
+	    public void checkImpact( IMonster4 monster, IEnergyBall energyBall) {
 
 	    }
 
-	    public void checkImpact( IMonster monster, IPurse purse) {
+	    public void checkImpact( IMonster1 monster, IPurse purse) {
+
+	    }
+	    
+	    public void checkImpact( IMonster2 monster, IPurse purse) {
+
+	    }
+	    
+	    public void checkImpact( IMonster3 monster, IPurse purse) {
+
+	    }
+	    
+	    public void checkImpact( IMonster4 monster, IPurse purse) {
 
 	    }
 
-	    public void checkImpact( IMonster monster, IDoorClosed doorClosed) {
+	    public void checkImpact( IMonster1 monster, IDoorClosed doorClosed) {
+
+	    }
+	    
+	    public void checkImpact( IMonster2 monster, IDoorClosed doorClosed) {
+
+	    }
+	    
+	    public void checkImpact( IMonster3 monster, IDoorClosed doorClosed) {
+
+	    }
+	    
+	    public void checkImpact( IMonster4 monster, IDoorClosed doorClosed) {
 
 	    }
 
-	    public void checkImpact( IMonster monster, IDoorOpened doorOpened) {
+	    public void checkImpact( IMonster1 monster, IDoorOpened doorOpened) {
+
+	    }
+	    
+	    public void checkImpact( IMonster2 monster, IDoorOpened doorOpened) {
+
+	    }
+	    
+	    public void checkImpact( IMonster3 monster, IDoorOpened doorOpened) {
+
+	    }
+	    
+	    public void checkImpact( IMonster4 monster, IDoorOpened doorOpened) {
 
 	    }
 
