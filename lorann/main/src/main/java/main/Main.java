46bb4bd879b8e6f599038bf17f6e8a543922e6bf
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-06-04 16:11:22 +0200

    suite

--- lorann/main/src/main/java/main/Main.java
+++ lorann/main/src/main/java/main/Main.java
@@ -21,7 +21,7 @@ public abstract class Main {
      *            the arguments
      */
     public static void main(final String[] args) {
-        final ControllerFacade controller = new ControllerFacade(new run(), new ModelFacade());
+        final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
 
         try {
             controller.start();
