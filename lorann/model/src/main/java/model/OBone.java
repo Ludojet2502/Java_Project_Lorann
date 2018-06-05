824f6c89b785ae773a5adc39ccce94e2ca593bcb
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-06-05 17:07:00 +0200

    Update OBone.java

--- lorann/model/src/main/java/model/OBone.java
+++ lorann/model/src/main/java/model/OBone.java
@@ -6,16 +6,17 @@ import model.IOBone;
  * 
  * @author piers
  *
+ *The class which create the object OBone
  */
 public class OBone extends Bone implements IOBone {
 	private static final char SPRITE = 'R';
 	
 	/**
-	 * 
+	 * The method which called the sprite
 	 */
 	OBone(){
 		super(SPRITE);
-		System.out.println("Création de l'objet OBone");
+		System.out.println("CrÃ©ation de l'objet OBone");
 	}
 
 }
