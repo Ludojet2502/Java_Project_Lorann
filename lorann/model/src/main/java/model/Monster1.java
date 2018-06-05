849a6e7728bed18c05ef3e0947ca960bee1d18cb
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-06-05 17:02:12 +0200

    Update Monster1.java

--- lorann/model/src/main/java/model/Monster1.java
+++ lorann/model/src/main/java/model/Monster1.java
@@ -5,16 +5,17 @@ import model.IMonster1;
 /**
  * 
  * @author piers
+ *The class wihch create the object Monster1
  *
  */
 public class Monster1 extends Motion implements IMonster1 {
 	private static final char SPRITE = 'W';
 	
 	/**
-	 * 
+	 * The method which called the sprite
 	 */
 	Monster1(){
 		super(SPRITE);
-		System.out.println("Création de l'objet Monster1");
+		System.out.println("CrÃ©ation de l'objet Monster1");
 	}
 }
