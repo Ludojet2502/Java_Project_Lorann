1a10326af943aebc622e321ead1c021fec0fd3e7
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-06-05 17:10:57 +0200

    Update VBone.java

--- lorann/model/src/main/java/model/VBone.java
+++ lorann/model/src/main/java/model/VBone.java
@@ -6,16 +6,17 @@ import model.IVBone;
  * 
  * @author piers
  *
+ *The class which create the object VBone
  */
 public class VBone extends Bone implements IVBone{
 	private static final char SPRITE = 'V';
 	
 	/**
-	 * 
+	 * The method which called the sprite
 	 */
 	VBone(){
 		super(SPRITE);
-		System.out.println("Création de l'objet VBone");
+		System.out.println("CrÃ©ation de l'objet VBone");
 	}
 
 }
