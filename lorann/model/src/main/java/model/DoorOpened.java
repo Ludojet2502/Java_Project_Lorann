aeba36278f8cbbaccb27d6be6ad9e94a07bf2edc
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-06-05 16:58:29 +0200

    Update DoorOpened.java

--- lorann/model/src/main/java/model/DoorOpened.java
+++ lorann/model/src/main/java/model/DoorOpened.java
@@ -6,15 +6,16 @@ import model.IDoorOpened;
  * 
  * @author piers
  *
+ *The class wich create the object DoorOpened
  */
 public class DoorOpened extends Door implements IDoorOpened{
 	private static final char SPRITE = ' ';
 	
 	/**
-	 * 
+	 * The method which called the sprite
 	 */
 	DoorOpened(){
 		super(SPRITE);
-		System.out.println("Création de l'objet DoorOpened");
+		System.out.println("CrÃ©ation de l'objet DoorOpened");
 	}
 }
