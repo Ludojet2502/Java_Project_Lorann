2ed14b6d21c3144f0b31637cf0ed30352fdc47ef
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-06-05 16:55:51 +0200

    Update Door.java

--- lorann/model/src/main/java/model/Door.java
+++ lorann/model/src/main/java/model/Door.java
@@ -3,17 +3,18 @@ package model;
 /**
  * 
  * @author piers
- *
+ * The class which create the object EnergyBall
  */
 public class Door extends Motionless{
 	Door(char sprite){
 		super(sprite);
-		System.out.println("Création de l'objet Door");
+		System.out.println("CrÃ©ation de l'objet Door");
 	}
 	
 	/**
 	 * 
 	 * @return
+	 * The method which return the state
 	 */
 	private boolean state() {
 		return this.state();
