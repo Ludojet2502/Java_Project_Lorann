92bdebbef1faf52721f7d3e171e8c742cd41a484
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-06-05 17:02:49 +0200

    Update Monster2.java

--- lorann/model/src/main/java/model/Monster2.java
+++ lorann/model/src/main/java/model/Monster2.java
@@ -5,15 +5,16 @@ import model.IMonster2;
  * 
  * @author piers
  *
+ *The class which create the object Monster2
  */
 public class Monster2 extends Motion implements IMonster2 {
 	private static final char SPRITE = 'X';
 	
 	/**
-	 * 
+	 * The method which called the sprite
 	 */
 	Monster2(){
 		super(SPRITE);
-		System.out.println("Création de l'objet Monster2");
+		System.out.println("CrÃ©ation de l'objet Monster2");
 	}
 }
