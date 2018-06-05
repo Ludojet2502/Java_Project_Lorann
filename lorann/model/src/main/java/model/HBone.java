0a08cc5e2c4cdbadb2247adb6641bd1ba69dc3ca
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-06-05 17:00:08 +0200

    Update HBone.java

--- lorann/model/src/main/java/model/HBone.java
+++ lorann/model/src/main/java/model/HBone.java
@@ -6,16 +6,17 @@ import model.IHBone;
  * 
  * @author piers
  *
+ *The class which create the object HBone
  */
 public class HBone extends Bone implements IHBone{
 	private static final char SPRITE = 'H';
 	
 	/**
-	 * 
+	 * The method which called the sprite
 	 */
 	HBone(){
 		super(SPRITE);
-		System.out.println("Création de l'objet HBone");
+		System.out.println("CrÃ©ation de l'objet HBone");
 	}
 
 }
