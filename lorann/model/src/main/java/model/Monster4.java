25c928a49d2b3b0b1ba49e436e54a85b08920411
Ludojet2502 <33137563+Ludojet2502@users.noreply.github.com>
2018-06-05 17:04:03 +0200

    Update Monster4.java

--- lorann/model/src/main/java/model/Monster4.java
+++ lorann/model/src/main/java/model/Monster4.java
@@ -6,15 +6,16 @@ import model.IMonster4;
  * 
  * @author piers
  *
+ *The class which create the object Monster4
  */
 public class Monster4 extends Motion implements IMonster4 {
 	private static final char SPRITE = 'Z';
 	
 	/**
-	 * 
+	 * The method which called the sprite
 	 */
 	Monster4(){
 		super(SPRITE);
-		System.out.println("Création de l'objet Monster4");
+		System.out.println("CrÃ©ation de l'objet Monster4");
 	}
 }
